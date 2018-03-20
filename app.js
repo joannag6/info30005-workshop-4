66f32a470292c8533ee8b31473718dec2f4658fc
Joanna <joannag6@gmail.com>
2018-03-20 23:28:09 +1100

    Used ejs to render html content

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const app = express();
 
 /* Route responses to client requests */
 app.get('/', function(req,res) {
-  res.send("Hello World");
+  // res.send("Hello World");
+  res.render("home.ejs");
 });
 app.get('/bye', function(req,res) {
   res.send("Goodbye world");
 });
 app.get('/users', function(req,res) {
-  res.send(db);
+  res.render("users.ejs", {
+    users: db
+  });
 });
 app.get('/users/:id', function(req,res) {
-  res.send(db[req.params.id]);
+  res.render("singleuser.ejs", {
+    user: db[req.params.id]
+  });
 });
-
 
 /* Starts the server */
 app.listen(3000, function() {
